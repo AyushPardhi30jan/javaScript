ade1d1aba7c5201196fa44633a5f32dd69b4ab76
AyushPardhi30jan <143348722+AyushPardhi30jan@users.noreply.github.com>
2024-08-15 05:40:00 +0000

    variables and dataTypes

---
+++ learnJs/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as nwe version
+
+// alert(3+3) used on cosole(browser) not used in nodejs
+
+// number =>2^53
+// bigint
+// string ""
+// boolean true/false
+// null standalone value
+// undefined not assigned yet
+// symbol uniqueness
+
+// object
+console.log(typeof null); // type of null returns object
+console.log(typeof undefined); //type of undefined is undefined
---
+++ learnJs/variables.js
@@ -0,0 +1,11 @@
+const accId=13
+let accEmail="ayush@gmail.com"
+var password="1234"
+accCity="Surat"
+
+// accId=19  values of const cant be changed
+accEmail="ayu@gmail.com"
+accCity="Mumbai"
+// console.log(accId);
+// avoid var because of block scope and functional scope
+console.table([accId,accEmail,password,accCity]);
